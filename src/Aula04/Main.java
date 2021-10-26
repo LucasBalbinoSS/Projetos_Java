5300e8e110f3cb9a0befd064b527f78eca863617
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2021-10-26 08:25:14 -0300

    Exercícios Java - Aula 04

---
+++ src/Aula04/Main.java
@@ -0,0 +1,12 @@
+package Aula04;
+
+public class Main {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("Faber Castel", "Verde", 1.5f);  //Criando com construtor
+
+        System.out.println();
+
+        Caneta c2 = new Caneta("BIC", "Vermelha", 0.5f);
+
+    }
+}
