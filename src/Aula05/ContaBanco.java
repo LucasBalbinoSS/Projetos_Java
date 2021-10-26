5872f0fb0302a4f1f09aff770d747e5cadca2e17
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2021-10-26 08:25:32 -0300

    Exercícios Java - Aula 05

---
+++ src/Aula05/ContaBanco.java
@@ -0,0 +1,60 @@
+package Aula05;
+
+import java.util.Scanner;
+
+public class ContaBanco {
+
+    // Entrada de dados pelo input do usuário //
+    Scanner input = new Scanner(System.in);
+
+    // Atributos //
+    public String num_conta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    // Getters & Setters //
+    public String getNum_conta() {
+        return this.num_conta;
+    }
+
+    public void setNum_conta() {
+        this.num_conta = input.next();
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setTipo(String t) {
+        this.tipo = t;
+    }
+
+    public String getDono() {
+        return this.dono;
+    }
+
+    public void setDono(String d) {
+        this.dono = d;
+    }
+
+    public float getSaldo() {
+        return this.saldo;
+    }
+
+    public void setSaldo(float s) {
+        this.saldo = s;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean s) {
+        this.status = s;
+    }
+
+    // Métodos //
+
+}
