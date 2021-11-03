9bdddb4ea28b95d2e999256e418fd66c8b982ba6
Lucas Balbino <lucas.balbinosantos@hotmail.com>
2021-11-03 10:37:26 -0300

    Exercícios Java - Fatec

--- Fatec/src/Aula_14_03_11_21/GravarArquivo.java
+++ Fatec/src/Aula_14_03_11_21/GravarArquivo.java
@@ -2,13 +2,21 @@ package Aula_14_03_11_21;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class GravarArquivo {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um texto: ");
+        String texto = sc.nextLine();
+
         try {
             FileWriter myWriter = new FileWriter("filename.txt");
-            myWriter.write("Arquivos em Java podem ser complicados, mas são divertidos!\nPor enquanto é só!");
+
+            myWriter.write(texto);
             myWriter.close();
+
             System.out.println("Gravação feita com sucesso!");
 
         } catch (IOException e) {
